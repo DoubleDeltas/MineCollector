88e820d1bac241d11f56d1b715de6ccd7038fb8f
Bonwoong <thesmartist@naver.com>
2023-03-18 00:50:32 +0900

    fix: 도감이 우클릭 이외에서도 열리는 문제

--- src/main/java/com/doubledeltas/minecollector/event/OpenBookEventListener.java
+++ src/main/java/com/doubledeltas/minecollector/event/OpenBookEventListener.java
@@ -7,6 +7,7 @@ import com.doubledeltas.minecollector.util.SoundUtil;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
@@ -17,6 +18,9 @@ public class OpenBookEventListener implements Listener {
         ItemStack collectionBook = MineCollector.getPlugin().getItemManager().getItem(StaticItem.COLLECTION_BOOK);
         Player player = e.getPlayer();
         PlayerInventory playerInventory = player.getInventory();
+
+        if (!(e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK))
+            return;
 
         if (!playerInventory.getItemInMainHand().equals(collectionBook)
                 && !playerInventory.getItemInOffHand().equals(collectionBook))
