415b2a812765871870b4c95c8d6bef1e6736899e
Bonwoong <thesmartist@naver.com>
2023-04-30 07:48:26 +0900

    fix: db-autosave period constraint modified

--- src/main/java/com/doubledeltas/minecollector/data/AutoSaver.java
+++ src/main/java/com/doubledeltas/minecollector/data/AutoSaver.java
@@ -14,6 +14,6 @@ public class AutoSaver {
 
         task = SCHEDULER.runTaskTimer(MineCollector.getInstance(), () -> {
 
-        }, 0, dbConfig.getAutosavePeriod())
+        }, 0, dbConfig.getAutosavePeriod());
     }
 }
