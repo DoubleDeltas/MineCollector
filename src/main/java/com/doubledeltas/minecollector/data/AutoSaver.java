92cfba7b517c7243c44ef5b48699eed1fc18af78
Bonwoong <thesmartist@naver.com>
2023-04-30 07:46:42 +0900

    feat: config.yml comment added

---
+++ src/main/java/com/doubledeltas/minecollector/data/AutoSaver.java
@@ -0,0 +1,19 @@
+package com.doubledeltas.minecollector.data;
+
+import com.doubledeltas.minecollector.MineCollector;
+import com.doubledeltas.minecollector.config.chapter.DBChapter;
+import org.bukkit.scheduler.BukkitScheduler;
+import org.bukkit.scheduler.BukkitTask;
+
+public class AutoSaver {
+    public static final BukkitScheduler SCHEDULER = MineCollector.getInstance().getServer().getScheduler();
+    public static BukkitTask task = null;
+
+    public void start() {
+        DBChapter dbConfig = MineCollector.getInstance().getMcolConfig().getDb();
+
+        task = SCHEDULER.runTaskTimer(MineCollector.getInstance(), () -> {
+
+        }, 0, dbConfig.getAutosavePeriod())
+    }
+}
