b9d3a3b144f24710bdbb6569ac782af6fb09255e
Bonwoong <thesmartist@naver.com>
2023-04-29 17:32:08 +0900

    feat: MessageUtil.announce/announceRaw

--- src/main/java/com/doubledeltas/minecollector/util/MessageUtil.java
+++ src/main/java/com/doubledeltas/minecollector/util/MessageUtil.java
@@ -1,6 +1,7 @@
 package com.doubledeltas.minecollector.util;
 
 import com.doubledeltas.minecollector.MineCollector;
+import com.doubledeltas.minecollector.config.AnnouncementTarget;
 import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.command.CommandSender;
@@ -33,11 +34,11 @@ public class MessageUtil {
 
     /**
      * 플레이어 또는 콘솔에게 마인콜렉터 메시지를 보냅니다.
-     * @param target 수신자
+     * @param subject 수신자
      * @param msg 메시지
      */
-    public static void send(CommandSender target, String msg) {
-        target.sendMessage(MSG_PREFIX + msg);
+    public static void send(CommandSender subject, String msg) {
+        subject.sendMessage(MSG_PREFIX + msg);
     }
 
     /**
@@ -49,16 +50,29 @@ public class MessageUtil {
     }
 
     /**
+     * {@link AnnouncementTarget}에 따라 메시지를 보냅니다.
+     * @param target 타겟 설정
+     * @param subject 주체({@link AnnouncementTarget#SELF}일 때 수신자)
+     * @param msg 메시지
+     */
+    public static void announce(AnnouncementTarget target, CommandSender subject, String msg) {
+        switch (target) {
+            case ALL_PLAYERS -> broadcast(msg);
+            case SELF -> send(subject, msg);
+        }
+    }
+
+    /**
      * 플레이어 또는 콘솔에게 마인콜렉터 접두어와 함께 채팅 컴포넌트를 보냅니다.
-     * @param target 수신자
+     * @param subject 주체(수신자)
      * @param components 보낼 컴포넌트들
      */
-    public static void sendRaw(CommandSender target, BaseComponent... components) {
+    public static void sendRaw(CommandSender subject, BaseComponent... components) {
         List<BaseComponent> list = new ArrayList<>();
         list.add(MSG_PREFIX_COMPONENT);
         list.addAll(Arrays.asList(components));
 
-        target.spigot().sendMessage(list.toArray(new BaseComponent[0]));
+        subject.spigot().sendMessage(list.toArray(new BaseComponent[0]));
     }
 
     /**
@@ -71,5 +85,18 @@ public class MessageUtil {
         list.addAll(Arrays.asList(components));
 
         MineCollector.getInstance().getServer().spigot().broadcast(list.toArray(new BaseComponent[0]));
+    }
+
+    /**
+     * {@link AnnouncementTarget}에 따라 채팅 컴포넌트를 보냅니다.
+     * @param target 타겟 설정
+     * @param subject 주체({@link AnnouncementTarget#SELF}일 때 수신자)
+     * @param msg 메시지
+     */
+    public static void announceRaw(AnnouncementTarget target, CommandSender subject, BaseComponent... components) {
+        switch (target) {
+            case ALL_PLAYERS -> broadcastRaw(components);
+            case SELF -> sendRaw(subject, components);
+        }
     }
 }
