7c1292e87a7ebce21d9973b176d3bc48b1d7e87b
Bonwoong <thesmartist@naver.com>
2023-04-29 12:05:37 +0900

    feat: config loading complete

--- src/main/java/com/doubledeltas/minecollector/config/Config.java
+++ src/main/java/com/doubledeltas/minecollector/config/Config.java
@@ -6,13 +6,12 @@ import com.doubledeltas.minecollector.config.chapter.DBChapter;
 import com.doubledeltas.minecollector.config.chapter.GameChapter;
 import com.doubledeltas.minecollector.config.chapter.ScoringChapter;
 import com.doubledeltas.minecollector.util.MessageUtil;
-import lombok.*;
-import org.yaml.snakeyaml.DumperOptions;
+import com.doubledeltas.minecollector.util.SpaceToCamelPropertyUtils;
+import lombok.Data;
 import org.yaml.snakeyaml.LoaderOptions;
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.constructor.Constructor;
-import org.yaml.snakeyaml.introspector.PropertyUtils;
-import org.yaml.snakeyaml.representer.Representer;
+import org.yaml.snakeyaml.introspector.BeanAccess;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -20,21 +19,22 @@ import java.io.FileReader;
 
 @Data
 public class Config {
-    public static LoaderOptions LOADER_OPTIONS = new LoaderOptions();
-    public static Representer REPRESENTER = new Representer(new DumperOptions());
-    public static File CONFIG_PATH = new File(MineCollector.getPlugin().getDataFolder(), "config.yml");
-
-    static {
-        PropertyUtils propUtils = new PropertyUtils();
-        propUtils.setAllowReadOnlyProperties(true);
-        REPRESENTER.setPropertyUtils(propUtils);
-    }
-
     private boolean             enabled;
     private ScoringChapter      scoring;
     private AnnouncementChapter announcement;
     private GameChapter         game;
     private DBChapter           db;
+
+
+    public static Constructor YAML_CONSTRUCTOR;
+    public static File CONFIG_PATH = new File(MineCollector.getPlugin().getDataFolder(), "config.yml");
+
+    static {
+        LoaderOptions loaderOptions = new LoaderOptions();
+        loaderOptions.setEnumCaseSensitive(false);
+        YAML_CONSTRUCTOR = new Constructor(Config.class, loaderOptions);
+        YAML_CONSTRUCTOR.setPropertyUtils(new SpaceToCamelPropertyUtils());
+    }
 
     public static Config load() {
         // 파일이 없으면 기본 콘피그 파일 생성
@@ -44,7 +44,8 @@ public class Config {
             MessageUtil.log("기본 콘피그 파일 생성됨!");
         }
 
-        Yaml yaml = new Yaml(new Constructor(Config.class, LOADER_OPTIONS), REPRESENTER);
+        Yaml yaml = new Yaml(YAML_CONSTRUCTOR);
+        yaml.setBeanAccess(BeanAccess.FIELD);
         try {
             Config config = yaml.load(new FileReader(CONFIG_PATH));
             MessageUtil.log("콘피그 불러옴!");
