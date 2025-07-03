0c8ade78efc2cee3ed1d9618bc2081b71748d2d3
Bonwoong <thesmartist@naver.com>
2025-07-03 20:33:47 +0900

    Fix: guide command

--- src/main/java/com/doubledeltas/minecollector/command/impl/GuideCommand.java
+++ src/main/java/com/doubledeltas/minecollector/command/impl/GuideCommand.java
@@ -1,18 +1,15 @@
 package com.doubledeltas.minecollector.command.impl;
 
-import com.doubledeltas.minecollector.MineCollector;
 import com.doubledeltas.minecollector.command.CommandRoot;
-import com.doubledeltas.minecollector.lang.LangManager;
-import com.doubledeltas.minecollector.lang.MessageKey;
 import com.doubledeltas.minecollector.util.MessageUtil;
 import com.doubledeltas.minecollector.util.SoundUtil;
-import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.ClickEvent;
-import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+
+import static com.doubledeltas.minecollector.lang.LangManager.translateToComponents;
 
 public final class GuideCommand extends CommandRoot {
     public static final String GUIDE_LINK = "https://github.com/DoubleDeltas/MineCollector/blob/main/README.md";
@@ -22,7 +19,10 @@ public final class GuideCommand extends CommandRoot {
 
     @Override
     public boolean onRawCommand(CommandSender sender, Command command, String label, String[] args) {
-        BaseComponent[] hereComponents = plugin.getLangManager().translate(MessageKey.of("command.guide.here"));
+        BaseComponent[] hereComponents = translateToComponents("command.guide.here");
+        for (BaseComponent hereComponent : hereComponents)
+            hereComponent.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, GUIDE_LINK));
+
         MessageUtil.send(sender, "command.guide.message", (Object[]) hereComponents);
 
         if (sender instanceof Player player)
